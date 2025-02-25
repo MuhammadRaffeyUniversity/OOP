83ead48c32f954329b9df0805cda50edca4fa362
Muhammad Raffey <70153209@student.uol.edu.pk>
2025-02-25 04:32:13 +0000

    Add character counting function to analyze occurrences in a string

---
+++ Lab/25-Feb/3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int charCount(char c, char arr[], int i)
+{
+    arr[i] = tolower(arr[i]);
+    if (arr[i] == '\0')
+        return 0;
+    if (arr[i] == c)
+        return 1 + charCount(c, arr, i + 1);
+    if (arr[i] != c)
+    {
+        return charCount(c, arr, i + 1);
+    }
+
+    return charCount(c, arr, i + 1);
+}
+
+int main()
+{
+    char arr[] = "I am an Agentic AI Developer and I use Python to build AI Agents";
+    cout << charCount('a', arr, 0) << endl;
+    return 0;
+}
