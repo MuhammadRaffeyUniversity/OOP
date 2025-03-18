13b15d249232ea8f87d4a9ee865268caea409744
Muhammad Raffey <70153209@student.uol.edu.pk>
2025-03-18 03:28:29 +0000

    added task 1 of todays lab

---
+++ Lab/18-March/1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class ShoppingCart
+{
+private:
+    int cartID;
+
+public:
+    double *totalprice;
+    ShoppingCart(int id, double price)
+    {
+        cartID = id;
+        totalprice = new double(price);
+    }
+
+    ShoppingCart(ShoppingCart &other)
+    {
+        cartID = other.cartID;
+        totalprice = other.totalprice;
+        // totalprice = new double(*other.totalprice); // Deep copy
+    }
+
+    void display()
+    {
+        cout << "Cart ID: " << cartID << ", Total Price: $" << *totalprice << endl;
+    }
+};
+
+int main()
+{
+    double price = 59.99;
+    ShoppingCart cart1(101, price);
+    ShoppingCart cart2 = cart1;
+
+    cart1.display();
+    cart2.display();
+
+    *cart1.totalprice = 79.99;
+    cout << "\nAfter changing price through cart1:" << endl;
+    cart1.display();
+    cart2.display();
+
+    return 0;
+}
