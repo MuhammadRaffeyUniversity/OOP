b4e1e824067d390d358453429073822156d2dc3e
Muhammad Raffey <70153209@student.uol.edu.pk>
2025-05-13 05:23:16 +0000

    task 6 done

---
+++ Lab/Lab-09/6.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class NegativeMarks
+{
+    string errormsg;
+
+public:
+    NegativeMarks() : errormsg("Marks can not be negative.") {}
+    NegativeMarks(string msg) : errormsg(msg) {}
+    string what() { return errormsg; }
+};
+int main()
+{
+    try
+    {
+        int marks;
+        cout << "Enter your marks: ";
+        cin >> marks;
+
+        if (marks < 0)
+        {
+            throw NegativeMarks();
+        }
+        cout << "Your marks are: " << marks << endl;
+    }
+    catch (NegativeMarks e)
+    {
+        cout << e.what() << endl;
+    }
+    return 0;
+}
